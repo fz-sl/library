81579bd4172b3d73b21cafc9e0c5ee48588431d6
Faraz <faraz67fz@gmail.com>
2020-10-27 10:37:34 +0330

    create routes for books

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::post('/books','BooksController@store');
+Route::patch('/books/{Book}','BooksController@update');*/
+Route::resource('/books','BooksController');
